af28ca8ad6772ea7096f4360b8812a155c1d9b42
cbohan <connor.bohan@gmail.com>
2021-01-29 17:59:38 -0800

    Take 2

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rigidBody;
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 moveVector = Vector3.zero;
+        if (Input.GetKey(KeyCode.W)) moveVector.z++;
+        if (Input.GetKey(KeyCode.S)) moveVector.z--;
+        if (Input.GetKey(KeyCode.D)) moveVector.x++;
+        if (Input.GetKey(KeyCode.A)) moveVector.x--;
+        moveVector.Normalize();
+
+        rigidBody.velocity = moveVector;
+    }
+}
