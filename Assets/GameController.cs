e4cad8bf36ce41ef3a289791deac074fcb46411b
PusherCoder <bendegn@gmail.com>
2021-01-30 21:34:01 -0800

    Extending Boxes

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -41,19 +41,36 @@ public class GameController : MonoBehaviour
 
         // Generate the list of boxes for all three stages
         foreach (Transform child in Stage1Boxes.transform) stage1BoxChildren[i++] = child.gameObject;
+        i = 0;
         foreach (Transform child in Stage2Boxes.transform) stage2BoxChildren[i++] = child.gameObject;
+        i = 0;
         foreach (Transform child in Stage3Boxes.transform) stage3BoxChildren[i++] = child.gameObject;
 
         // Concatenate the arrays based on the current stage
         GameObject[] boxArray = new GameObject[Stage1Boxes.transform.childCount + Stage2Boxes.transform.childCount + Stage3Boxes.transform.childCount];
+        int boxArraySize = Stage1Boxes.transform.childCount;
         stage1BoxChildren.CopyTo(boxArray, 0);
-        if (gameStage > 1) stage2BoxChildren.CopyTo(boxArray, Stage1Boxes.transform.childCount);
-        if (gameStage > 2) stage3BoxChildren.CopyTo(boxArray, (Stage1Boxes.transform.childCount + Stage2Boxes.transform.childCount));
+        if (gameStage > 1)
+        {
+            boxArraySize += Stage2Boxes.transform.childCount;
+            stage2BoxChildren.CopyTo(boxArray, Stage1Boxes.transform.childCount);
+        }
+        if (gameStage > 2)
+        {
+            boxArraySize += Stage3Boxes.transform.childCount;
+            stage3BoxChildren.CopyTo(boxArray, (Stage1Boxes.transform.childCount + Stage2Boxes.transform.childCount));
+        }
+        
+        // Activate all the boxes
+        for (i = 0; i < boxArraySize; i++)
+        {
+            boxArray[i].SetActive(true);
+        }
 
         // Deactive the appropriate number of boxes
-        for ( i = 0; i < boxesToKill; )
+        for (i = 0; i < boxesToKill; )
         {
-            int boxToKill = Random.Range(0, boxArray.Length);
+            int boxToKill = Random.Range(0, boxArraySize);
             if (boxArray[boxToKill].activeInHierarchy == true)
             {
                 boxArray[boxToKill].SetActive(false);
