b7280e33948325513d24418512aa30567c400ae8
cbohan <connor.bohan@gmail.com>
2021-01-30 22:08:03 -0800

    Foundry menu lets you refill ammo

--- Assets/UI/FoundryMenu.cs
+++ Assets/UI/FoundryMenu.cs
@@ -5,6 +5,9 @@ public class FoundryMenu : MonoBehaviour
 {
     public static FoundryMenu Instance;
 
+    //Resources
+    public int MetalScrap = 20;
+
     //Pistol
     public int PistolAmmo = 25;
     public int MaxPistolAmmo = 25;
@@ -17,6 +20,7 @@ public class FoundryMenu : MonoBehaviour
     [Header("Menus")]
     [SerializeField] private GameObject pistolMenu;
     [SerializeField] private GameObject shotgunMenu;
+    [SerializeField] private Text resourcesText;
 
     [Header("Pistol")]
     [SerializeField] private Text pistolAmmoText;
@@ -37,15 +41,27 @@ public class FoundryMenu : MonoBehaviour
 
     private void Update()
     {
+        //Resources
+        resourcesText.text = $"metal scrap: {MetalScrap}";
+
         //Pistol buttons
         pistolAmmoText.text = $"pistol ammo ({PistolAmmo}/{MaxPistolAmmo})";
-        refillPistolButton.interactable = PistolAmmo < MaxPistolAmmo;
-        fiveAmmoPistolButton.interactable = PistolAmmo <= MaxPistolAmmo - 5;
+        refillPistolButtonText.text = $"refill ({(MaxPistolAmmo - PistolAmmo)} metal scrap)";
+        refillPistolButton.interactable = (PistolAmmo < MaxPistolAmmo) && 
+            (MaxPistolAmmo - PistolAmmo <= MetalScrap);
+        fiveAmmoPistolButton.interactable = (PistolAmmo <= MaxPistolAmmo - 5) &&
+            (MetalScrap >= 5);
 
         //Shotgun buttons
         shotgunAmmoText.text = $"shotgun ammo ({ShotgunAmmo}/{MaxShotgunAmmo})";
-        refillShotgunButton.interactable = ShotgunAmmo < MaxShotgunAmmo;
-        fiveAmmoShotgunButton.interactable = ShotgunAmmo <= MaxShotgunAmmo - 5;
+        refillShotgunButtonText.text = $"refill ({(MaxShotgunAmmo - ShotgunAmmo) * 3} metal scrap)";
+        refillShotgunButton.interactable = ShotgunAmmo < MaxShotgunAmmo &&
+            ((MaxShotgunAmmo - ShotgunAmmo) * 3 <= MetalScrap);
+        fiveAmmoShotgunButton.interactable = ShotgunAmmo <= MaxShotgunAmmo - 5 && 
+            (MetalScrap >= 15);
+
+        //Cheatyness
+        if (Input.GetKeyDown(KeyCode.Backspace) && Application.isEditor) MetalScrap += 10;
     }
 
     public void ClickPistolMenu()
@@ -60,5 +76,29 @@ public class FoundryMenu : MonoBehaviour
         shotgunMenu.SetActive(true);
     }
 
+    public void ClickRefillPistolAmmo()
+    {
+        int newBullets = MaxPistolAmmo - PistolAmmo;
+        MetalScrap -= newBullets;
+        PistolAmmo = MaxPistolAmmo;
+    }
 
+    public void ClickFivePistolAmmo()
+    {
+        MetalScrap -= 5;
+        PistolAmmo += 5;
+    }
+
+    public void ClickRefillShotgunAmmo()
+    {
+        int newBullets = MaxShotgunAmmo - ShotgunAmmo;
+        MetalScrap -= newBullets * 3;
+        ShotgunAmmo = MaxShotgunAmmo;
+    }
+
+    public void ClickFiveShotgunAmmo()
+    {
+        MetalScrap -= 15;
+        ShotgunAmmo += 5;
+    }
 }
