92ef61e97569d11e32f3ed6ba6ce4c621e10563a
PusherCoder <bendegn@gmail.com>
2021-01-31 08:56:21 -0800

    You've Got Mail

---
+++ Assets/UI/Computer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Computer : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup prompt;
+    private bool playerInTrigger = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerInTrigger)
+        {
+            prompt.alpha = 1f;
+        }
+        else
+            prompt.alpha = 0f;
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<PlayerController>() != null)
+            playerInTrigger = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.GetComponent<PlayerController>() != null)
+            playerInTrigger = false;
+    }
+}
