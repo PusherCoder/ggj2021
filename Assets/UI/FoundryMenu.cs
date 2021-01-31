8248df620729748862cc3739c86925730bb87d13
cbohan <connor.bohan@gmail.com>
2021-01-30 21:09:44 -0800

    Got rid of AmplifyShader thingy

---
+++ Assets/UI/FoundryMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FoundryMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject pistolMenu;
+    [SerializeField] private GameObject shotgunMenu;
+
+    [SerializeField] private Button refillPistolButton;
+    [SerializeField] private Text refillPistolButtonText;
+
+    [SerializeField] private Button refillShotgunButton;
+    [SerializeField] private Text refillShotgunButtonText;
+
+    public void ClickPistolMenu()
+    {
+        pistolMenu.SetActive(true);
+        shotgunMenu.SetActive(false);
+    }
+
+    public void ClickShotgunMenu()
+    {
+        pistolMenu.SetActive(false);
+        shotgunMenu.SetActive(true);
+    }
+}
