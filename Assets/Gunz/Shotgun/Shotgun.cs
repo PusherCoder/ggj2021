1822fe093d46fdd006419d0022327864f6e0963a
cbohan <connor.bohan@gmail.com>
2021-01-30 19:23:11 -0800

    Foundry take 2

--- Assets/Gunz/Shotgun/Shotgun.cs
+++ Assets/Gunz/Shotgun/Shotgun.cs
@@ -37,13 +37,14 @@ public class Shotgun : Gun
         PlaySFX(Random.Range(.95f, 1f), Random.Range(1f, 1.05f));
         DoRecoil();
 
-        for (int i = 0; i < 7; i++)
+        for (int i = 0; i < 12; i++)
         {
-            Vector2 offset = Random.insideUnitCircle * .02f;
+            Vector2 offset = Random.insideUnitCircle * .03f;
             GunRaycastHit hit = DoRaycast(offset.x, offset.y);
             if (hit.Hit)
             {
-                SparksPsSpawner.OnSpawnSparksParticles.Invoke(hit.Position, hit.Normal);
+                if (i % 3 == 0)
+                    SparksPsSpawner.OnSpawnSparksParticles.Invoke(hit.Position, hit.Normal);
                 IDamagable damagable = hit.Object.GetComponent<IDamagable>();
                 if (damagable != null) damagable.TakeDamage(15);
             }
