2291cac15e143a3b5142cfa26a3a09085e866492
PusherCoder <bendegn@gmail.com>
2021-01-30 20:16:16 -0800

    Boxes Go Bye Bye

---
+++ Assets/BoxController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collided with me!");
+        transform.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
