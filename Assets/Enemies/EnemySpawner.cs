04c15461d1c4a85e7f509ee1543fd19c8b92f0a0
cbohan <connor.bohan@gmail.com>
2021-01-31 12:33:40 -0800

    Spawn'n de enemeez

---
+++ Assets/Enemies/EnemySpawner.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public Zone SpawnerZone; 
+
+    [SerializeField] private GameObject visual;
+    [SerializeField] private SkeletonMissingArm skeletonMissingArmPrefab;
+    [SerializeField] private SkeletonMage skeletonMagePrefab;
+
+    private void Awake()
+    {
+        visual.SetActive(false);
+    }
+
+    public void SpawnSkeletonMissingArm()
+    {
+        SkeletonMissingArm skeletonMissingArm = Instantiate(skeletonMissingArmPrefab);
+        skeletonMissingArm.transform.position = transform.position;
+    }
+
+    public void SpawnSkeletonMage()
+    {
+        SkeletonMage skeletonMage = Instantiate(skeletonMagePrefab);
+        skeletonMage.transform.position = transform.position;
+    }
+}
+
+public enum Zone { Zone1, Zone2, Zone3 }
---
+++ Assets/Enemies/SpawnManager.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    List<EnemySpawner> zone1Spawners = new List<EnemySpawner>();
+    List<EnemySpawner> zone2Spawners = new List<EnemySpawner>();
+    List<EnemySpawner> zone3Spawners = new List<EnemySpawner>();
+
+    private void Awake()
+    {
+        EnemySpawner[] spawners = FindObjectsOfType<EnemySpawner>();
+        foreach (EnemySpawner spawner in spawners)
+        {
+            switch (spawner.SpawnerZone)
+            {
+                case Zone.Zone1:
+                    zone1Spawners.Add(spawner);
+                    break;
+                case Zone.Zone2:
+                    zone2Spawners.Add(spawner);
+                    break;
+                case Zone.Zone3:
+                    zone3Spawners.Add(spawner);
+                    break;
+            }
+        }
+
+        SpawnInZone1(2);
+    }
+
+    private void SpawnInZone1(int count)
+    {
+        List<EnemySpawner> spawners = new List<EnemySpawner>();
+        foreach (EnemySpawner spawner in zone1Spawners)
+            spawners.Add(spawner);
+        Spawn(spawners, count);
+    }
+
+    private void SpawnInZone1And2(int count)
+    {
+        List<EnemySpawner> spawners = new List<EnemySpawner>();
+        foreach (EnemySpawner spawner in zone1Spawners)
+            spawners.Add(spawner);
+        foreach (EnemySpawner spawner in zone2Spawners)
+            spawners.Add(spawner);
+        Spawn(spawners, count);
+    }
+
+    private void SpawnInZone1And2And3(int count)
+    {
+        List<EnemySpawner> spawners = new List<EnemySpawner>();
+        foreach (EnemySpawner spawner in zone1Spawners)
+            spawners.Add(spawner);
+        foreach (EnemySpawner spawner in zone2Spawners)
+            spawners.Add(spawner);
+        foreach (EnemySpawner spawner in zone3Spawners)
+            spawners.Add(spawner);
+        Spawn(spawners, count);
+    }
+
+    private void Spawn(List<EnemySpawner> spawners, int count)
+    {
+        Shuffle(spawners);
+
+        for (int i = 0; i < Mathf.Min(count, spawners.Count); i++)
+        {
+            int type = Random.Range(0, 2);
+            if (type == 0)
+                spawners[i].SpawnSkeletonMissingArm();
+            else
+                spawners[i].SpawnSkeletonMage();
+        }
+    }
+
+    private void Shuffle(List<EnemySpawner> list)
+    {
+        System.Random rng = new System.Random();
+
+        int n = list.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = rng.Next(n + 1);
+            EnemySpawner value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+        }
+    }
+}
