3e88b2a8a78753b8aaa2e11838048367d29e9607
PusherCoder <bendegn@gmail.com>
2021-01-31 16:07:57 -0800

    Allow People to Leave

---
+++ Assets/Scenes/EndCard.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndCard : MonoBehaviour
+{
+    public Text TitleText;
+
+    private bool inflate = true;
+    private int slowItDown = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (slowItDown-- == 0)
+        {
+            if (inflate)
+            {
+                if (TitleText.fontSize++ > 85) inflate = false;
+            }
+            else
+            {
+                if (TitleText.fontSize-- < 55) inflate = true;
+            }
+            slowItDown = 5;
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Application.Quit();
+        }
+
+    }
+}
